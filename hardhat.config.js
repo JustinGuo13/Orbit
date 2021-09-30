47bb23b471e13cfc04731c817efcecd0098f20a6
Peng Yang Guo <pyangguo@gmail.com>
2021-09-30 14:30:37 -0700

    Setup

---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+require('@nomiclabs/hardhat-waffle');
+
+const fs = require('fs');
+const privateKey = fs.readFileSync('.secret').toString();
+const projectId = '7dcf778da7424655a4e65ae51a08514b';
+
+module.exports = {
+	networks: {
+		hardhat: {
+			chainId: 1337,
+		},
+		mumbai: {
+			url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+			accounts: [privateKey],
+		},
+		mainnet: {
+			url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+			accounts: [privateKey],
+		},
+	},
+	solidity: '0.8.4',
+};
