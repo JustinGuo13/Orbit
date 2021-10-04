b047956317f9c1fcbdbb08f7dc54bf62527c937c
Peng Yang Guo <pyangguo@gmail.com>
2021-10-04 15:32:35 -0700

    Implement front end

---
+++ config.js
@@ -0,0 +1,2 @@
+export const nftaddress = '0xa1bB1cF6B9A5B77c8a670925601378Cb57B7C824';
+export const nftmarketaddress = '0x4DdE6C81a4948Cdb6faC4cA2bc6D23BB9005CD53';
---
+++ scripts/deploy.js
@@ -0,0 +1,20 @@
+const hre = require('hardhat');
+
+async function main() {
+	const NFTMarket = await hre.ethers.getContractFactory('NFTMarket');
+	const nftMarket = await NFTMarket.deploy();
+	await nftMarket.deployed();
+	console.log('nftMarket deployed to:', nftMarket.address);
+
+	const NFT = await hre.ethers.getContractFactory('NFT');
+	const nft = await NFT.deploy(nftMarket.address);
+	await nft.deployed();
+	console.log('nft deployed to:', nft.address);
+}
+
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
