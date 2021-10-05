3204fa42df2e3d0685af2367af7d078c57741ef3
Peng Yang Guo <pyangguo@gmail.com>
2021-10-05 14:41:29 -0700

    Deploy to Mumbai test net

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
 	return (
 		<div>
 			<nav className="border-b p-6">
-				<p className="text-4xl font-bold">Metaverse Marketplace</p>
+				<p className="text-4xl font-bold">Swift Marketplace</p>
 				<div className="flex mt-4">
 					<Link href="/">
 						<a className="mr-4 text-green-500">Home</a>
